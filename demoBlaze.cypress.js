7939050f534f75f2c406c5c38f4ff8f8b885860e
Olha Panchenko <100361581+olgapanchenko85@users.noreply.github.com>
2024-08-06 10:21:06 +0300

    demoBlaze.cypress.js

--- demoBlaze.cypress.js
+++ demoBlaze.cypress.js
@@ -1,1 +1,14 @@
 /// <reference types="cypress" />
+const { faker } = require('@faker-js/faker');
+
+const testData = {
+  username: faker.person.firstName(),
+  country: faker.location.country(),
+  password: faker.internet.password(),
+  alertMessage: 'Product added'
+};
+
+describe('Demoblaze', () => {
+  it('should provide an ability to register new account', () => {
+   
+});
